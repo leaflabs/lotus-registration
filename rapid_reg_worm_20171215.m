c861887220bfca9984bc711eb4fd8cd0216b94f0
Justin Kinney <jkinney@leaflabs.com>
2018-02-13 09:58:48 -0500

    rapid_reg_worm_20171215.m works. Output mat files look good as tifs.

--- rapid_reg_worm_20171215.m
+++ rapid_reg_worm_20171215.m
@@ -1,53 +1,105 @@
-% from /om/user/jkinney/DLFM/worm/20171215/interpolate/Recon3D_solver_1_FrameNumber_0001__20180123_110051.log
-%
-% trans = [45.54140 30.9580 52.98630],
-% rot = [-1.519490       00       00] 
-% final offset = [0.3213920 -8.609540 1.986310]
-% centroid: [45.2200 39.5675 51]
-% clip: [450 250 150 350 0 0]
-%
-% from /om/user/jkinney/DLFM/worm/20171215/interpolate/Recon3D_solver_1_FrameNumber_1000__20180123_110443.log
-%
-% trans = [37.56430 48.03390 55.08210], 
-% rot = [-1.517870       00       00]  
-% final offset = [-7.655650 8.46640 4.082070]
-% centroid: [45.2200 39.5675 51]
-% clip: [450 250 150 350 0 0]
+clear all;
+close all;
 
+%% load parameters
+param = init_param();
+
+%% setup parameters
+[~,hostname] = system('hostname')
+param.hostname = strtrim(hostname);
+
+setup_par
+
+if ~isempty(strfind(param.hostname, 'Justins-Mac'))
+        param.ppath = '/Users/justin/Desktop/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = [param.ppath '/worm/20171215'];
+        param.opath = param.ipath;
+        param.inter = [param.ipath '/interpolate/'];
+elseif ~isempty(strfind(param.hostname, 'willis'))
+        param.ppath = '/home/jkinney/Desktop/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = [param.ppath '/worm/20171215'];
+        param.opath = param.ipath;
+        param.inter = [param.ipath '/interpolate/'];
+else
+        param.ppath = '/om/user/jkinney/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = '/om/project/boyden/DualLensLightField/12_15_17_40X_worm/video 1';
+        param.opath = '/om/scratch/jkinney/worm/20171215';
+        param.inter = [param.ipath '/interpolate/'];
+end
+param.inputFilePath1 = [param.ipath '/horizontal/Reconstructed/'];
+param.inputFilePath2 = [param.ipath '/vertical/Reconstructed/'];
+
+param.savePath = [param.opath '/registration/'];
+param.voxel_x = 0.323/2; % um
+param.voxel_y = 0.323/2; % um
+param.voxel_z = 2.0;     % um
 
 m = 1;
 n = 1000;
-trans1 = [45.54140 30.9580 52.98630];
-trans2 = [37.56430 48.03390 55.08210];
-rot1 = [-1.519490       00       00];
-rot2 = [-1.517870       00       00];
 
-for i=[1 1000]
-    clearvars -except i m n trans1 trans2 rot1 rot2;
-    close all;
-    %rpath = '/om/user/jkinney/DLFM';
-    rpath = '/Users/justin/Desktop/DLFM';
-    addpath(rpath);
-%     dpath = '/om/project/boyden/DualLensLightField/12_15_17_40X_worm/video 1';
-%     inputFilePath1 = [dpath '/horizontal/Reconstructed/'];
-%     inputFilePath2 = [dpath '/vertical/Reconstructed/'];
-%     savePath       = [rpath '/worm/20171215/registration/'];
-inputFilePath1 = [rpath '/worm/20171215/horizontal/Reconstructed/'];
-inputFilePath2 = [rpath '/worm/20171215/vertical/Reconstructed/'];
-savePath       = [rpath '/worm/20171215/registration/'];
+param.rapid = true;
 
-    clip    = [450 250 150 350 0 0];
-    voxel_x = 0.323/2; % um
-    voxel_y = 0.323/2; % um
-    voxel_z = 2.0; % um
-
-    rapid = true;
-    lfdisplay = false;
-    centroid = [45.2200 39.5675 51] ...
-        + [clip(1)*voxel_y clip(3)*voxel_x 0];
-    rot = rot1 + (rot1-rot2) * (i-m)/(n-m);
-    trans = trans1 + (trans1-trans2) * (i-m)/(n-m);
-    trans = trans + [clip(1)*voxel_y clip(3)*voxel_x 0];
-    inputFileName = {sprintf('Recon3D_solver_1_FrameNumber_%04d.mat',i)};
-    register
+%% read log files
+pathNpattern = sprintf('%s*FrameNumber_%04d_*.log',param.inter,m);
+logs = dir(pathNpattern);
+if isempty(logs)
+    disp('WTF?');
+    fprintf('%s\n',pathNpattern);
+    keyboard
 end
+content = fileread([logs(1).folder '/' logs(1).name]);
+
+eval(['transM = ' get_match('trans',content)]);
+eval([  'rotM = ' get_match('rot',content)]);
+eval([  'centroidM = ' get_match('centroid',content)]);
+eval([  'clipM = ' get_match('clip',content)]);
+
+pathNpattern = sprintf('%s*FrameNumber_%04d_*.log',param.inter,n);
+logs = dir(pathNpattern);
+if isempty(logs)
+    disp('WTF?');
+    fprintf('%s\n',pathNpattern);
+    keyboard
+end
+content = fileread([logs(1).folder '/' logs(1).name]);
+
+eval(['transN = ' get_match('trans',content)]);
+eval([  'rotN = ' get_match('rot',content)]);
+eval([  'centroidN = ' get_match('centroid',content)]);
+eval([  'clipN = ' get_match('clip',content)]);
+
+if ~isempty(find( (clipN-clipM) ~= 0))
+    disp('WTF?');
+    keyboard
+end
+param.clip = clipN; % equal to clipM
+if ~isempty(find( (centroidN-centroidM) ~= 0))
+    disp('WTF?');
+    keyboard
+end
+param.centroid = centroidN ...
+        + [param.clip(1)*param.voxel_y param.clip(3)*param.voxel_x 0];
+
+for i=[param.m:param.n]
+    param.rot = rotM + (rotN-rotM) * (i-m)/(n-m);
+    tmp_trans = transM + (transN-transM) * (i-m)/(n-m);
+    param.trans = tmp_trans + [ param.clip(1)*param.voxel_y  ...
+        param.clip(3)*param.voxel_x   param.clip(5)*param.voxel_z ];
+    param.inputFileName = {sprintf('Recon3D_solver_1_FrameNumber_%04d.mat',i)};
+    
+    param
+    
+    register(param)
+end
+
+function out = get_match (str, content)
+expr = ['[^\n]* ' str ':[^\n]*'];
+match = regexp(content,expr,'match');
+s = regexp(match,'\[.*\]','match');
+out = s{1}{1};
+end
+
+
