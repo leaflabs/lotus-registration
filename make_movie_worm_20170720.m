cf211e28f8ff552df758e98a3b8a426f06332b31
Justin Kinney <jkinney@leaflabs.com>
2018-02-14 15:33:55 -0500

    Introducing the first make_movie script.

---
+++ make_movie_worm_20170720.m
@@ -0,0 +1,49 @@
+clear all;
+close all;
+
+%% load parameters
+param = init_param();
+
+%% setup parameters
+[~,hostname] = system('hostname')
+param.hostname = strtrim(hostname);
+
+setup_par
+
+if ~isempty(strfind(param.hostname, 'Justins-Mac'))
+        param.ppath = '/Users/justin/Desktop/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = [param.ppath '/worm/20170720'];
+        param.opath = param.ipath;
+        param.inter = [param.ipath '/interpolate/'];
+elseif ~isempty(strfind(param.hostname, 'willis'))
+        param.ppath = '/home/jkinney/Desktop/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = [param.ppath '/worm/20170720'];
+        param.opath = param.ipath;
+        param.inter = [param.ipath '/interpolate/'];
+else
+        param.ppath = '/om/user/jkinney/DLFM';
+        addpath([param.ppath '/lotus-registration']);
+        param.ipath = '/om/user/ehoseini/MyData/DuallensLightField/7_20_17/video_1'
+        param.opath = '/om/scratch/Mon/jkinney/worm/20170720';
+        param.spath = '/om/user/jkinney/DLFM/worm/20170720'
+        param.inter = [param.spath '/interpolate/'];
+end
+
+param.inputFilePath1 = [param.ipath '/horizontal/Reconstructed/'];
+param.inputFilePath2 = [param.ipath '/vertical/Reconstructed/'];
+
+param.savePath = [param.opath '/registration/'];
+
+param.m = 13;
+param.n = 300;
+
+
+
+
+dataOutFileName='worm_20170720';
+
+param
+
+genVideo(param)
