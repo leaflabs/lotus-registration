de09f45eda53a8dabb2e7d406e3263170b592441
Justin Kinney <jkinney@leaflabs.com>
2018-02-14 06:36:40 -0500

    Added resume option to rapid_reg for three scripts.

--- rapid_reg_worm_20170720.m
+++ rapid_reg_worm_20170720.m
@@ -56,12 +56,14 @@ param.voxel_z = 2.0;     % um
 
 param.m = 13;
 param.n = 300;
+%next = param.m;
+next = 228;
 
 param.rapid = true;
 
 param = read_logs(param);
 
-for i=[param.m:param.n]
+for i=[next:param.n]
     param.rot = param.rotM + (param.rotN-param.rotM) * (i-param.m)/(param.n-param.m);
     tmp_trans = param.transM + (param.transN-param.transM) * (i-param.m)/(param.n-param.m);
     param.trans = tmp_trans + [ param.clip(1)*param.voxel_y  ...
