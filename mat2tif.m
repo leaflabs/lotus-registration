aab34ccbf7d49c07975a0be769e6a81e4493e647
Justin Kinney <jkinney@leaflabs.com>
2018-02-15 10:51:15 -0500

    Derive output filename from input filename.

--- mat2tif.m
+++ mat2tif.m
@@ -1,14 +1,15 @@
-function mat2tif( f, o )
+function mat2tif( f )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+o = [f(1:end-4) '.tif'];
 load(f);
-
-if ~exist('XguessSAVE1','var')
+if exist('XguessSAVE1','var')
+    save_vol(XguessSAVE1, '', o);
+elseif exist('Xvolume','var')
+    save_vol(Xvolume, '', o);
+else
     keyboard
 end
-
-save_vol(XguessSAVE1, '', o);
-
 end
 
 
