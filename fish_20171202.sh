8d2c031e6dfb58973891ef4073f30a200af86399
Justin Kinney <jkinney@leaflabs.com>
2018-02-15 08:40:17 -0500

    Finally adding top level scripts for launching registration jobs on openmind.

---
+++ fish_20171202.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -p om_all_nodes           # partition (queue)
+#SBATCH -N 1                      # number of nodes
+#SBATCH -n 20                     # number of cores
+#SBATCH --mem 128000              # memory pool for all cores
+#SBATCH -t 2-00:00                # time (D-HH:MM)
+#SBATCH -o %x.slurm.%N.%j.out        # STDOUT
+#SBATCH -e %x.slurm.%N.%j.err        # STDERR
+#SBATCH --mail-type=END,FAIL      # notifications for job done & fail
+#SBATCH --mail-user=jkinney@mit.edu # send-to address
+module add mit/matlab/2016b
+cd /om/user/jkinney/DLFM/lotus-registration
+export TZ=America/New_York
+matlab -nodisplay -nodesktop -nosplash -r "run('master_reg_fish_20171202.m');disp('FINISHED');exit"
+#matlab -nodisplay -nodesktop -nosplash -r "run('rapid_reg_worm_20171215.m');exit"
+#matlab -nodisplay -nodesktop -nosplash -r "eghbal=false;data3DPath='/om/user/jkinney/DLFM/worm/20171215/registration/';savePath='/om/user/jkinney/DLFM/worm/20171215/';filePattern='*multiply*.mat';dataOutFileName='worm_20171215';run('genVideo.m');exit"
---
+++ submit.sh
@@ -0,0 +1,11 @@
+sbatch bead_20180109.sh
+sbatch worm_20180103.sh
+sbatch fish_20171221.sh
+sbatch worm_20171215.sh
+sbatch fish_20171202.sh
+sbatch worm_20171117.sh
+sbatch worm_20171114.sh
+sbatch worm_20171012.sh
+sbatch bead_20171011.sh
+sbatch bead_20170915.sh
+sbatch worm_20170720.sh
