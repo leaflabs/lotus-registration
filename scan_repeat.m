6503307ee0acafa1e88d4001ed18918186029123
Justin Kinney <jkinney@leaflabs.com>
2018-02-26 10:43:09 -0500

    Added historams.

--- scan_repeat.m
+++ scan_repeat.m
@@ -1,7 +1,8 @@
 clear all;
 close all;
 
-ppath = '/Users/justin/Desktop/DLFM/fish/20171221/repeat';
+%ppath = '/Users/justin/Desktop/DLFM/fish/20171221/repeat';
+ppath = '/home/jkinney/Desktop/DLFM/fish/20171221/repeat';
 pathNpattern = sprintf('%s/*.log',ppath);
 logs = dir(pathNpattern);
 if isempty(logs)
@@ -21,9 +22,140 @@ for i=1:n
     r = [r;out.rot];
 end
 
-a = size(o);
+plot_raw(o, r, out, ppath);
+plot_histo(o, r, ppath);
 
-%% ofset
+
+
+
+%%
+function plot_histo (o,r, ppath)
+
+a = size(o);
+% offset
+h = figure('units','normalized','outerposition',[0 0 1 1]);
+
+subplot(2,3,1);
+histogram(o(:,1));
+xlabel('offset dim 1 (um)');
+x = xlim;
+del = x(2)-x(1);
+
+subplot(2,3,2);
+histogram(o(:,2));
+xlabel('offset dim 2 (um)');
+x = xlim;
+ndel = x(2)-x(1);
+if ndel > del
+    del = ndel;
+end
+
+subplot(2,3,3);
+histogram(o(:,3));
+xlabel('offset dim 3 (um)');
+x = xlim;
+ndel = x(2)-x(1);
+if ndel > del
+    del = ndel;
+end
+
+%
+%
+%
+
+subplot(2,3,1);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+subplot(2,3,2);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+subplot(2,3,3);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+%
+%
+%
+subplot(2,3,4);
+histogram(r(:,1));
+xlabel('rotation dim 1 (radians)');
+x = xlim;
+del = x(2)-x(1);
+
+subplot(2,3,5);
+histogram(r(:,2));
+xlabel('rotation dim 2 (radians)');
+x = xlim;
+ndel = x(2)-x(1);
+if ndel > del
+    del = ndel;
+end
+
+subplot(2,3,6);
+histogram(r(:,3));
+xlabel('rotation dim 3 (radians)');
+x = xlim;
+ndel = x(2)-x(1);
+if ndel > del
+    del = ndel;
+end
+
+
+%
+%
+%
+
+subplot(2,3,4);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+subplot(2,3,5);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+subplot(2,3,6);
+x = xlim;
+ndel = x(2)-x(1);
+m = (del-ndel)/2;
+xlim([x(1)-m x(2)+m]);
+
+%
+%
+%
+
+ax1 = axes('Position',[0 0 1 1],'Visible','off');
+axes(ax1);
+str = sprintf('Repeatability of trajectory of simulated annealing (N = %d)',a(1));
+text(0.25,0.97,str,'FontSize',8,'Color',[0 0 0],'Interpreter','none');
+
+str = [ppath '/histograms.png'];
+print(h,str,'-dpng');
+
+
+end
+
+
+
+
+
+
+%%
+function plot_raw (o,r, out, ppath)
+
+a = size(o);
+% offset
 h = figure;
 
 subplot(1,3,1);
@@ -84,12 +216,12 @@ ylim([y(1)-m y(2)+m]);
 ax1 = axes('Position',[0 0 1 1],'Visible','off');
 axes(ax1);
 str = sprintf('Repeatability of trajectory of simulated annealing (N = %d)',a(1));
-text(0.25,0.97,str,'FontSize',12,'Color',[0 0 0],'Interpreter','none');
+text(0.25,0.97,str,'FontSize',8,'Color',[0 0 0],'Interpreter','none');
 
 str = [ppath '/distribution_offset.png'];
 print(h,str,'-dpng');
 
-%% rotation
+% rotation
 h = figure;
 
 subplot(1,3,1);
@@ -150,13 +282,17 @@ ylim([y(1)-m y(2)+m]);
 ax1 = axes('Position',[0 0 1 1],'Visible','off');
 axes(ax1);
 str = sprintf('Repeatability of trajectory of simulated annealing (N = %d)',a(1));
-text(0.25,0.97,str,'FontSize',12,'Color',[0 0 0],'Interpreter','none');
+text(0.25,0.97,str,'FontSize',8,'Color',[0 0 0],'Interpreter','none');
 
 str = [ppath '/distribution_rotation.png'];
 print(h,str,'-dpng');
 
-function out = read_log (f)
+end
+
+
+
 %% read log file
+function out = read_log (f)
 out = [];
 
 content = fileread(f);
