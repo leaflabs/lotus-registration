085a1c9228fe6d7bbd848bd40bfccea853b3c17b
Justin Kinney <jkinney@leaflabs.com>
2018-03-07 06:46:25 -0500

    Updated pull_results...sh. Note that a comment is used to select between /om/user and /om/scratch.

--- pull_results_from_openmind.sh
+++ pull_results_from_openmind.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
 declare -a arr=(
-"fish/20171221/repeat"
+"bead/20170915/registration"
+"bead/20171011/registration"
+"bead/20180109/registration"
+"fish/20171202/interpolate"
+"fish/20171221/interpolate"
+"fish/20180123/interpolate"
+"worm/20170720/interpolate"
+"worm/20171012/interpolate"
+"worm/20171114/registration"
+"worm/20171117/registration"
+"worm/20171215/interpolate"
+"worm/20180103/registration"
 )
-
-#declare -a arr=(
-#"bead/20170915/registration"
-#"bead/20171011/registration"
-#"bead/20180109/registration"
-#"fish/20171202/interpolate"
-#"fish/20171221/interpolate"
-#"worm/20170720/interpolate"
-#"worm/20171012/interpolate"
-#"worm/20171114/registration"
-#"worm/20171117/registration"
-#"worm/20171215/interpolate"
-#"worm/20180103/registration"
-#)
 
 if [[ $(hostname -s) = willis ]]; then
     echo 'this is willis'
-    LPATH=/home/jkinney/Desktop/DLFM
+    LPATH=/home/jkinney/Desktop/DDLFM
 elif [[ $(hostname -s) = *Justins-Mac* ]]; then
     echo 'this is mac air'
-    LPATH=/Users/justin/Desktop/DLFM
+    LPATH=/Users/justin/Desktop/DDLFM
 else
     echo unknown
     exit
 fi
 
-RPATH=jkinney@openmind.mit.edu:/om/scratch/Mon/jkinney
+#RPATH=jkinney@openmind.mit.edu:/om/scratch/Mon/jkinney
+RPATH=jkinney@openmind.mit.edu:/om/user/jkinney/DLFM
 
 for i in "${arr[@]}"; do
     echo "##"
