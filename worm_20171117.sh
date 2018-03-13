95125d19544532ea276105aa897970fddcd472db
Justin Kinney <jkinney@leaflabs.com>
2018-03-13 09:51:14 -0400

    Increased memory request by 64GB to 192GB

--- worm_20171117.sh
+++ worm_20171117.sh
@@ -3,7 +3,7 @@
 #SBATCH -p om_all_nodes           # partition (queue)
 #SBATCH -N 1                      # number of nodes
 #SBATCH -n 20                     # number of cores
-#SBATCH --mem 128000              # memory pool for all cores
+#SBATCH --mem 192000              # memory pool for all cores
 #SBATCH -t 2-00:00                # time (D-HH:MM)
 #SBATCH -o %x.slurm.%N.%j.out        # STDOUT
 #SBATCH -e %x.slurm.%N.%j.err        # STDERR
