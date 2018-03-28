0e4d3753b695713601f27abc754a097ce55347bb
Justin Kinney <jkinney@leaflabs.com>
2018-03-28 07:55:57 -0400

    In read_next.m allow empty folder to signal fresh start.

--- read_next.m
+++ read_next.m
@@ -3,17 +3,23 @@ function next = read_next (ddir, prefix, suffix)
 pathNpattern = sprintf('%s%s*%s',ddir,prefix,suffix);
 logs = dir(pathNpattern);
 if isempty(logs)
-    disp('WTF?');
-    fprintf('%s\n',pathNpattern);
-    keyboard
-end
-
-v = 0;
-for i=1:numel(logs)
-    t = logs(i).name(1:end-4);
-    j = str2num(t(end-3:end));
-    if j>v
-        v = j;
+    fprintf('Warning. No matches found for %s\n',pathNpattern);
+    if ~exist(ddir,'dir')
+        fprintf('Folder does not exist:%s\n',ddir);
+        exit;
+    else
+        % folder found so assume fresh start
+        next = 1;
     end
+else
+    % process matches
+    v = 0;
+    for i=1:numel(logs)
+        t = logs(i).name(1:end-4);
+        j = str2num(t(end-3:end));
+        if j>v
+            v = j;
+        end
+    end
+    next = v+1;
 end
-next = v+1;
