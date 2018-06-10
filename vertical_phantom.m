92f67838a618ed7819e005d63c102f501675a4c6
Justin Kinney <jpkinney@gmail.com>
2018-06-10 19:58:41 -0400

    Never got vertical_phantom working

--- vertical_phantom.m
+++ vertical_phantom.m
@@ -5,9 +5,11 @@ close all;
 param = init_param();
 
 % without loss of generality
-param.voxel_x = 1;
-param.voxel_y = 1;
-param.voxel_z = 1;
+param.voxel_x = 0.5;
+param.voxel_y = 0.5;
+param.voxel_z = 4.0;
+
+param.interp = 8;
 
 [~,hostname] = system('hostname')
 
@@ -48,15 +50,17 @@ postfix = 'Recon3D_after_9_Mono_N15.mat';
 f = [ppath '/vertical/Reconstructed/' postfix];
 fprintf('Loading %s...\n',f);
 load(f,'XguessSAVE1');
-A = XguessSAVE1;
+%A = XguessSAVE1;
+A = interpolate (XguessSAVE1,param);
+fprintf('done\n');
 clear XguessSAVE1;
 
 mystr = sprintf('%s/%s_%s',savePath,postfix(1:end-4),timestamp);
 
-% param.rot = [0 0 0];
-% param.trans = [0 0 0];
-% 
-% transform(A, param, mystr);
+param.rot = [0 0 0];
+param.trans = [0 0 0];
+
+transform(A, param, mystr);
 
 param.rot = [pi/2 0 0];
 param.trans = [0 0 0];
@@ -126,18 +130,40 @@ transform(A, param, mystr);
 diary off;
 
 %%
+function out = interpolate (LFM, param)
+% initialize container of new size
+s = size(LFM);
+out = zeros(s(1),s(2),param.interp*s(3),class(LFM));
+boundary = param.interp/2 + 1;
+for i=1:param.interp*s(3)
+    if i < boundary
+        out(:,:,i) = LFM(:,:,1);
+    elseif i > (param.interp*s(3)-(boundary-1))
+        out(:,:,i) = LFM(:,:,end);
+    else
+        a = round((i-1)/param.interp);
+        b = a+1;
+        N = 2*param.interp;
+        fb = 1+2*mod(i-(param.interp-1),param.interp);
+        fa = N-fb;
+        out(:,:,i) = fa/N*LFM(:,:,a) + fb/N*LFM(:,:,b);
+    end
+end
+end
+
 function transform (A, param, mystr)
-pos = init_pos([1:numel(A)]', A, param);
+fprintf('initializing position matrix...\n');
+pos = mod_init_pos(A, param);
 param.centroid = calc_centroid(A,param);
 fprintf('Translating...\n');
-tmp = translate (pos, -param.centroid);
+pos = translate (pos, -param.centroid);
 fprintf('Rotating...\n');
-tmp = rotate (tmp, param.rot); % CHECK
+pos = rotate (pos, param.rot); % CHECK
+% fprintf('Translating...\n');
+% tmp = translate (tmp, param.centroid);
 fprintf('Translating...\n');
-tmp = translate (tmp, param.centroid+param.trans);
-%fprintf('Translating...\n');
-%pos = translate (tmp, param.trans);
-A = render (A, pos, [1:numel(A)]', param);
+pos = translate (pos, param.centroid+param.trans);
+A = render (A, pos, param);
 % save as MAT file
 outFile = sprintf('%s_rot_%d_%d_%d_trans_%d_%d_%d.mat',...
     mystr,...
@@ -163,7 +189,7 @@ end
 end
 
 %%
-function out = render (LFM1, chunk, linind, param)
+function out = render (LFM1, chunk, param)
 %
 % See function out = combine (LFM1, LFM2, chunk, linind, param)
 % chunk = x,y,z centroids of voxels in LFM2
@@ -184,9 +210,12 @@ b = max(abc_LFM1);
 fprintf('max of indices (x, y, z) = [%d %d %d]\n',b(1),b(2),b(3));
 
 %out = zeros(a);
-out = zeros(b);
+out = zeros(b,class(LFM1));
 
 for i=1:L
+    if abc_LFM1(i,1)<1 || abc_LFM1(i,2)<1 || abc_LFM1(i,3)<1
+        continue;
+    end
     out(abc_LFM1(i,1),abc_LFM1(i,2),abc_LFM1(i,3)) = LFM1(i);
 end
 out(1,1,1) = 0.5;
@@ -226,15 +255,44 @@ end
 
 %%
 function [out] = translate (pos, delta)
-D = ones(size(pos),'single').*delta;
-out = pos + D;
+out = pos + single(delta); 
 end
 
 %%
-function pos = init_pos (linear_i,LFM, param)
-[a,b,c] = ind2sub(size(LFM),linear_i);
-y = single( (a-0.5) * param.voxel_y );
-x = single( (b-0.5) * param.voxel_x );
-z = single( (c-0.5) * param.voxel_z );
-pos = [y x z];
+function pos = mod_init_pos (LFM, param)
+N = numel(LFM);
+M = 10;
+n = round(N/M);
+pos = zeros(N,3,'single');
+for i=1:M
+    start = 1 + (i-1)*n;
+    stop = start+n-1;
+    if stop>N
+        stop = N;
+    end
+    [a,b,c] = ind2sub(size(LFM),start:stop);
+    y = single( (a-0.5) * param.voxel_y );
+    clear a;
+    x = single( (b-0.5) * param.voxel_x );
+    clear b;
+    z = single( (c-0.5) * param.voxel_z );
+    clear c;
+    keyboard
+    pos(start:stop,:) = [y x z];
 end
+end
+
+%%
+% function pos = init_pos (LFM, param)
+% keyboard
+% [a,b,c] = ind2sub(size(LFM),1:numel(LFM));
+% keyboard
+% y = single( (a-0.5) * param.voxel_y );
+% clear a;
+% keyboard
+% x = single( (b-0.5) * param.voxel_x );
+% clear b;
+% z = single( (c-0.5) * param.voxel_z );
+% clear c;
+% pos = [y x z];
+% end
